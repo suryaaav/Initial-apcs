f15e694f75927021376997dfaff90b5525228934
Surya Varkali <118791448+suryaaav@users.noreply.github.com>
2025-08-12 09:26:00 -0700

    Add files via upload

---
+++ Initials/src/Main.java
@@ -0,0 +1,103 @@
+import kareltherobot.*;
+
+public class Main implements Directions{
+  public static void main(String[] args) {
+    World.setVisible(true);// allows us to see the run
+    World.setSize(13,13);
+    World.setDelay(7);
+
+    // The line below creates a Robot that we will refer to as r.  
+    // Find out what the numbers and direction do!
+    Robot r = new Robot(11,5,East,30);
+		
+    // examples of commands you can invoke on a Robot
+    r.move();// move one step in the direction it is facing
+    r.turnLeft();
+    r.putBeeper();
+    r.move();
+    r.turnLeft();
+    r.move();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.turnLeft();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+    r.turnLeft();
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+
+
+
+
+  
+
+
+    
+  
+
+  }
+}
